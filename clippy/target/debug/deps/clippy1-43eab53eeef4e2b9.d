e31f328995bb2de68c3dd73499d94ee1ef35b9ab
boozmedina <boozmedina@gmail.com>
2021-04-24 09:00:33 -0500

    quiz4

---
+++ clippy/target/debug/deps/clippy1-43eab53eeef4e2b9.d
@@ -0,0 +1,5 @@
+/home/boozmedina/rustlings/exercises/clippy/target/debug/deps/clippy1-43eab53eeef4e2b9.rmeta: clippy1.rs
+
+/home/boozmedina/rustlings/exercises/clippy/target/debug/deps/clippy1-43eab53eeef4e2b9.d: clippy1.rs
+
+clippy1.rs:
